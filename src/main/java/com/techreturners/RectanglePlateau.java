47c21e19a90de99a68f8af9337a1dcfb6bc270a6
dwright1989 <debbiewright@live.co.uk>
2023-02-11 17:32:34 +0000

    Comments and minimal pseudocode added to allow for adjustments to vehicles that are larger than 1 square

--- src/main/java/com/techreturners/RectanglePlateau.java
+++ src/main/java/com/techreturners/RectanglePlateau.java
@@ -11,12 +11,23 @@ public class RectanglePlateau extends Plateau{
         int x = vehicle.getPosition()[0];
         int y = vehicle.getPosition()[1];
         super.getGrid()[x][y] = vehicle;
+        if(vehicle.getSize()>1){
+            // Additional logic can be added here if/when larger vehicles implemented
+            // E.g. spans over two squares - check the direction and add vehicle on additional array elements
+            // So size == 2 at pos [0,1] and direction N would also be added to [1,1]
+        }
     }
 
     public void removeVehicle(Vehicle vehicle) {
         int x = vehicle.getPosition()[0];
         int y = vehicle.getPosition()[1];
         super.getGrid()[x][y] = null;
+        if(vehicle.getSize()>1){
+            // Additional logic can be added here if/when larger vehicles implemented
+            // E.g. spans over two squares - check the direction and remove vehicle on additional array elements
+            // So size == 2 at pos [0,1] and direction N would also be removed from [1,1]
+        }
+
     }
 
 }
