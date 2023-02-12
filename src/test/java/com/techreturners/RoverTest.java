34c0845f0d568264a98b33d7d9a765f575a3f50e
dwright1989 <debbiewright@live.co.uk>
2023-02-12 13:07:29 +0000

    All tests passing for turning left and right from all directions

--- src/test/java/com/techreturners/RoverTest.java
+++ src/test/java/com/techreturners/RoverTest.java
@@ -114,6 +114,62 @@ public class RoverTest {
         assertEquals(Direction.W, rover.getDirection());
     }
 
+    @Test
+    public void roverChangesRightFromNorth(){
+        Rover rover = new Rover(new int[]{2,3}, Direction.N);
+        rectanglePlateau.addVehicle(rover);
+        rover.changeDirection(Orientation.R);
+        assertEquals(Direction.E, rover.getDirection());
+    }
+
+    @Test
+    public void roverChangesLeftFromEast(){
+        Rover rover = new Rover(new int[]{2,3}, Direction.E);
+        rectanglePlateau.addVehicle(rover);
+        rover.changeDirection(Orientation.L);
+        assertEquals(Direction.N, rover.getDirection());
+    }
+
+    @Test
+    public void roverChangesRightFromEast(){
+        Rover rover = new Rover(new int[]{2,3}, Direction.E);
+        rectanglePlateau.addVehicle(rover);
+        rover.changeDirection(Orientation.R);
+        assertEquals(Direction.S, rover.getDirection());
+    }
+
+    @Test
+    public void roverChangesLeftFromSouth(){
+        Rover rover = new Rover(new int[]{2,3}, Direction.S);
+        rectanglePlateau.addVehicle(rover);
+        rover.changeDirection(Orientation.L);
+        assertEquals(Direction.E, rover.getDirection());
+    }
+
+    @Test
+    public void roverChangesRightFromSouth(){
+        Rover rover = new Rover(new int[]{2,3}, Direction.S);
+        rectanglePlateau.addVehicle(rover);
+        rover.changeDirection(Orientation.R);
+        assertEquals(Direction.W, rover.getDirection());
+    }
+
+    @Test
+    public void roverChangesLeftFromWest(){
+        Rover rover = new Rover(new int[]{2,3}, Direction.W);
+        rectanglePlateau.addVehicle(rover);
+        rover.changeDirection(Orientation.L);
+        assertEquals(Direction.S, rover.getDirection());
+    }
+
+    @Test
+    public void roverChangesRightFromWest(){
+        Rover rover = new Rover(new int[]{2,3}, Direction.W);
+        rectanglePlateau.addVehicle(rover);
+        rover.changeDirection(Orientation.R);
+        assertEquals(Direction.N, rover.getDirection());
+    }
+
 
     /*@Test
     public void canMoveRoverOneSpaceToTheLeft(){
