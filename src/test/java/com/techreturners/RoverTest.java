b91e8d073b2d690f847acbbcd7c44128af90d28c
dwright1989 <debbiewright@live.co.uk>
2023-02-12 11:48:54 +0000

    additional tests added for checking if can move in each direction when no space etc.

--- src/test/java/com/techreturners/RoverTest.java
+++ src/test/java/com/techreturners/RoverTest.java
@@ -16,11 +16,81 @@ public class RoverTest {
     @Test
     public void canMoveForwardWhenFacingNorthAndSpaceAvailable(){
         RectanglePlateau rectanglePlateau = new RectanglePlateau(5,5);
+        Rover rover = new Rover(new int[]{2,3}, Direction.N);
+        rectanglePlateau.addVehicle(rover);
+        rectanglePlateau.moveVehicle(rover);
+        assertArrayEquals(new int[]{2,4}, rover.getPosition());
+        assertEquals(rectanglePlateau.getGrid()[4][2], rover); // Access reversed
+    }
+
+    @Test
+    public void roverStaysInTheSamePlaceIfNoSpaceAvailableFacingNorth(){
+        RectanglePlateau rectanglePlateau = new RectanglePlateau(5,5);
         Rover rover = new Rover(new int[]{2,4}, Direction.N);
         rectanglePlateau.addVehicle(rover);
         rectanglePlateau.moveVehicle(rover);
         assertArrayEquals(new int[]{2,4}, rover.getPosition());
-        assertEquals(rectanglePlateau.getGrid()[4][2], rover); // Printing back to front for grid purposes so 2,4 == 4,2
+        assertEquals(rectanglePlateau.getGrid()[4][2], rover); // Access reversed
+    }
+
+    @Test
+    public void canMoveForwardWhenFacingEastAndSpaceAvailable(){
+        RectanglePlateau rectanglePlateau = new RectanglePlateau(5,5);
+        Rover rover = new Rover(new int[]{2,3}, Direction.E);
+        rectanglePlateau.addVehicle(rover);
+        rectanglePlateau.moveVehicle(rover);
+        assertArrayEquals(new int[]{3,3}, rover.getPosition());
+        assertEquals(rectanglePlateau.getGrid()[3][3], rover); // Access reversed
+    }
+
+    @Test
+    public void roverStaysInTheSamePlaceIfNoSpaceAvailableFacingEast(){
+        RectanglePlateau rectanglePlateau = new RectanglePlateau(5,5);
+        Rover rover = new Rover(new int[]{4,3}, Direction.E);
+        rectanglePlateau.addVehicle(rover);
+        rectanglePlateau.moveVehicle(rover);
+        assertArrayEquals(new int[]{4,3}, rover.getPosition());
+        assertEquals(rectanglePlateau.getGrid()[3][4], rover); // Access reversed
+    }
+
+    @Test
+    public void canMoveForwardWhenFacingWestAndSpaceAvailable(){
+        RectanglePlateau rectanglePlateau = new RectanglePlateau(5,5);
+        Rover rover = new Rover(new int[]{1,1}, Direction.W);
+        rectanglePlateau.addVehicle(rover);
+        rectanglePlateau.moveVehicle(rover);
+        assertArrayEquals(new int[]{0,1}, rover.getPosition());
+        assertEquals(rectanglePlateau.getGrid()[1][0], rover); // Access reversed
+    }
+
+    @Test
+    public void roverStaysInTheSamePlaceIfNoSpaceAvailableFacingWest(){
+        RectanglePlateau rectanglePlateau = new RectanglePlateau(5,5);
+        Rover rover = new Rover(new int[]{0,1}, Direction.W);
+        rectanglePlateau.addVehicle(rover);
+        rectanglePlateau.moveVehicle(rover);
+        assertArrayEquals(new int[]{0,1}, rover.getPosition());
+        assertEquals(rectanglePlateau.getGrid()[1][0], rover); // Access reversed
+    }
+
+    @Test
+    public void canMoveForwardWhenFacingSouthAndSpaceAvailable(){
+        RectanglePlateau rectanglePlateau = new RectanglePlateau(5,5);
+        Rover rover = new Rover(new int[]{0,1}, Direction.S);
+        rectanglePlateau.addVehicle(rover);
+        rectanglePlateau.moveVehicle(rover);
+        assertArrayEquals(new int[]{0,0}, rover.getPosition());
+        assertEquals(rectanglePlateau.getGrid()[0][0], rover); // Access reversed
+    }
+
+    @Test
+    public void roverStaysInTheSamePlaceIfNoSpaceAvailableFacingSouth(){
+        RectanglePlateau rectanglePlateau = new RectanglePlateau(5,5);
+        Rover rover = new Rover(new int[]{0,0}, Direction.S);
+        rectanglePlateau.addVehicle(rover);
+        rectanglePlateau.moveVehicle(rover);
+        assertArrayEquals(new int[]{0,0}, rover.getPosition());
+        assertEquals(rectanglePlateau.getGrid()[0][0], rover); // Access reversed
     }
 
 
